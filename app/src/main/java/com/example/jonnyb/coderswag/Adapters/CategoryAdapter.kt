af8554a0489dab591c1d90e7b798dcfa5f565d4c
Mohammad Zulqurnain <prodigiousguy@hotmail.com>
2018-05-25 21:11:14 +0500

    lesson-05n05

---
+++ app/src/main/java/com/example/jonnyb/coderswag/Adapters/CategoryAdapter.kt
@@ -0,0 +1,65 @@
+package com.example.jonnyb.coderswag.Adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import com.example.jonnyb.coderswag.Model.Category
+import com.example.jonnyb.coderswag.R
+
+/**
+ * Created by jonnyb on 8/21/17.
+ */
+class CategoryAdapter(context: Context, categories: List<Category>) : BaseAdapter(){
+
+    val context = context
+    val categories = categories
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val holder:ViewHolder
+        val categoryView: View
+
+        if(convertView == null){
+            categoryView = LayoutInflater.from(context).inflate(R.layout.category_list_item, null)
+            holder = ViewHolder()
+            holder.categoryImage = categoryView.findViewById(R.id.categoryImage)
+            holder.categoryName = categoryView.findViewById(R.id.categoryName)
+            println("I Exist for the first time!")
+            categoryView.tag = holder
+        } else {
+            holder = convertView.tag as ViewHolder
+            categoryView = convertView
+            println("GO green, recycle!")
+        }
+
+        println("Heavy Computing")
+
+        val category = categories[position]
+        val resourceId = context.resources.getIdentifier(category.image, "drawable", context.packageName)
+        holder.categoryImage?.setImageResource(resourceId)
+        println(resourceId)
+        holder.categoryName?.text = category.title
+        return categoryView
+    }
+
+    override fun getItem(position: Int): Any {
+        return categories[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return 0
+    }
+
+    override fun getCount(): Int {
+        return categories.count()
+    }
+
+    private class ViewHolder {
+        var categoryImage: ImageView? = null
+        var categoryName: TextView? = null
+        //var category: ImageView? = null
+    }
+}
---
+++ app/src/main/java/com/example/jonnyb/coderswag/Controller/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.jonnyb.coderswag.Controller
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import com.example.jonnyb.coderswag.Adapters.CategoryAdapter
+import com.example.jonnyb.coderswag.Model.Category
+import com.example.jonnyb.coderswag.R
+import com.example.jonnyb.coderswag.Services.DataService
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var adapter : CategoryAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        adapter = CategoryAdapter(this, DataService.categories)
+        categoryListView.adapter = adapter
+
+        categoryListView.setOnItemClickListener{ adapterView, view, i, l ->
+            val category = DataService.categories[i]
+            Toast.makeText(this, "You clicked on the ${category.title} cell",  Toast.LENGTH_SHORT)
+        }
+    }
+}
